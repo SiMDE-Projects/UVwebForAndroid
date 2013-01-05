d2708a2155d0a38a8a8a56e940ff9bfbe1af6680
Thomas <thomas.keunebroek@gmail.com>
2013-01-05 19:57:01 +0100

    Initial commit

---
+++ src/fr/utc/assos/uvweb/UVDetailFragment.java
@@ -0,0 +1,63 @@
+package fr.utc.assos.uvweb;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.actionbarsherlock.app.SherlockFragment;
+import fr.utc.assos.uvweb.dummy.DummyContent;
+
+/**
+ * A fragment representing a single UV detail screen. This fragment is either
+ * contained in a {@link UVListActivity} in two-pane mode (on tablets) or a
+ * {@link UVDetailActivity} on handsets.
+ */
+public class UVDetailFragment extends SherlockFragment {
+	/**
+	 * The fragment argument representing the item ID that this fragment
+	 * represents.
+	 */
+	public static final String ARG_ITEM_ID = "item_id";
+
+	/**
+	 * The dummy content this fragment is presenting.
+	 */
+	private DummyContent.DummyItem mItem;
+
+	/**
+	 * Mandatory empty constructor for the fragment manager to instantiate the
+	 * fragment (e.g. upon screen orientation changes).
+	 */
+	public UVDetailFragment() {
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		if (getArguments().containsKey(ARG_ITEM_ID)) {
+			// Load the dummy content specified by the fragment
+			// arguments. In a real-world scenario, use a Loader
+			// to load content from a content provider.
+			mItem = DummyContent.ITEM_MAP.get(getArguments().getString(
+					ARG_ITEM_ID));
+		}
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_uv_detail,
+				container, false);
+
+		// Show the dummy content as text in a TextView.
+		if (mItem != null) {
+            // TODO: debug code
+			//((TextView) rootView.findViewById(R.id.uv_detail))
+					//.setText(mItem.content);
+		}
+
+		return rootView;
+	}
+}
