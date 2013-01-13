8282cdbb724afed8e2ccf13f3fa91524b6b4785f
Thomas <thomas.keunebroek@gmail.com>
2013-01-13 22:53:39 +0100

    Improve previous commit's code

--- src/fr/utc/assos/uvweb/UVListFragment.java
+++ src/fr/utc/assos/uvweb/UVListFragment.java
@@ -35,6 +35,11 @@ public class UVListFragment extends SherlockListFragment {
 	 */
 	private int mActivatedPosition = ListView.INVALID_POSITION;
 
+    /**
+     * The associated ListView object
+     */
+    private ListView mListView;
+
 	/**
 	 * A callback interface that all activities containing this fragment must
 	 * implement. This mechanism allows activities to be notified of item
@@ -78,6 +83,8 @@ public class UVListFragment extends SherlockListFragment {
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 
+        mListView = getListView();
+
 		// Restore the previously serialized activated item position.
 		if (savedInstanceState != null
 				&& savedInstanceState.containsKey(STATE_ACTIVATED_POSITION)) {
@@ -85,8 +92,8 @@ public class UVListFragment extends SherlockListFragment {
 					.getInt(STATE_ACTIVATED_POSITION));
 		}
 
-        getListView().setFastScrollEnabled(true);
-        getListView().setFastScrollAlwaysVisible(true);
+        mListView.setFastScrollEnabled(true);
+        mListView.setFastScrollAlwaysVisible(true);
 	}
 
 	@Override
@@ -136,16 +143,16 @@ public class UVListFragment extends SherlockListFragment {
 	public void setActivateOnItemClick(boolean activateOnItemClick) {
 		// When setting CHOICE_MODE_SINGLE, ListView will automatically
 		// give items the 'activated' state when touched.
-		getListView().setChoiceMode(
+		mListView.setChoiceMode(
 				activateOnItemClick ? ListView.CHOICE_MODE_SINGLE
 						: ListView.CHOICE_MODE_NONE);
 	}
 
 	private void setActivatedPosition(int position) {
 		if (position == ListView.INVALID_POSITION) {
-			getListView().setItemChecked(mActivatedPosition, false);
+			mListView.setItemChecked(mActivatedPosition, false);
 		} else {
-			getListView().setItemChecked(position, true);
+			mListView.setItemChecked(position, true);
 		}
 
 		mActivatedPosition = position;
