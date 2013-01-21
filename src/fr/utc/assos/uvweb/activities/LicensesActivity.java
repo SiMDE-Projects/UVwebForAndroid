8199d396b96d5d8df3937f879c44b7d79e732d79
Thomas <thomas.keunebroek@gmail.com>
2013-01-21 23:49:40 +0100

    Implement Licenses Activity

--- src/fr/utc/assos/uvweb/activities/LicensesActivity.java
+++ src/fr/utc/assos/uvweb/activities/LicensesActivity.java
@@ -1,6 +1,10 @@
 package fr.utc.assos.uvweb.activities;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import com.actionbarsherlock.view.Window;
+import fr.utc.assos.uvweb.R;
 
 /**
  * An simple "Licenses" activity, whose aim it to present the open source projects
@@ -8,8 +12,24 @@ import android.os.Bundle;
  */
 public class LicensesActivity extends UVwebActivity {
     public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         super.onCreate(savedInstanceState);
 
-        // TODO: add layout
+        setContentView(R.layout.licenses_activity);
+
+        WebView webView = (WebView) findViewById(R.id.web_view);
+        webView.setWebViewClient(new UVwebClient());
+
+        setSupportProgressBarIndeterminateVisibility(true);
+
+        webView.loadUrl("file:///android_asset/licenses.html");
+    }
+
+    private class UVwebClient extends WebViewClient {
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            setSupportProgressBarIndeterminateVisibility(false);
+        }
     }
 }
