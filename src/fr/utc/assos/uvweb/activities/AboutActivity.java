c78c97d1644c4ac1f061951d08b6b0b562f366e2
Thomas <thomas.keunebroek@gmail.com>
2013-01-22 21:38:39 +0100

    Implement AboutActivity layout

--- src/fr/utc/assos/uvweb/activities/AboutActivity.java
+++ src/fr/utc/assos/uvweb/activities/AboutActivity.java
@@ -1,6 +1,9 @@
 package fr.utc.assos.uvweb.activities;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+import fr.utc.assos.uvweb.R;
 
 /**
  * An simple "About" activity. Here we are supposed to introduce ourselves as well as the project
@@ -9,6 +12,11 @@ public class AboutActivity extends UVwebActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // TODO: add layout
+        setContentView(R.layout.activity_about);
+
+        ((TextView) findViewById(R.id.credits)).setText(Html.fromHtml(getResources().getString(R.string.credits)));
+
+        // TODO: click on buttons
+        // TODO: line separator
     }
 }
