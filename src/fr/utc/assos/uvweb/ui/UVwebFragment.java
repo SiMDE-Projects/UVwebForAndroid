86665fa8937faddc950efdbf19d627513f5c48e6
Thomas <thomas.keunebroek@gmail.com>
2013-04-27 18:13:11 +0200

    Improve network crouton messages management

--- src/fr/utc/assos/uvweb/ui/UVwebFragment.java
+++ src/fr/utc/assos/uvweb/ui/UVwebFragment.java
@@ -3,6 +3,7 @@ package fr.utc.assos.uvweb.ui;
 import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import de.keyboardsurfer.android.widget.crouton.Crouton;
+import de.keyboardsurfer.android.widget.crouton.LifecycleCallback;
 import fr.utc.assos.uvweb.R;
 import fr.utc.assos.uvweb.util.ConnectionUtils;
 
@@ -10,14 +11,30 @@ import fr.utc.assos.uvweb.util.ConnectionUtils;
  * Base Fragment that must be extended by any {@link SherlockFragment} that has a UI.
  */
 public abstract class UVwebFragment extends SherlockFragment {
+	private static boolean mIsNetworkCroutonDisplayed = false;
 	protected void handleNetworkError() {
 		handleNetworkError(getSherlockActivity());
 	}
 
 	protected void handleNetworkError(SherlockFragmentActivity context) {
-		Crouton.cancelAllCroutons();
-		Crouton.makeText(context,
-				context.getString(R.string.network_error_message),
-				ConnectionUtils.NETWORK_ERROR_STYLE).show();
+		if (!mIsNetworkCroutonDisplayed) {
+			mIsNetworkCroutonDisplayed = true;
+			final Crouton networkCrouton = Crouton.makeText(context,
+					context.getString(R.string.network_error_message),
+					ConnectionUtils.NETWORK_ERROR_STYLE);
+
+			networkCrouton.setLifecycleCallback(new LifecycleCallback() {
+				@Override
+				public void onDisplayed() {
+				}
+
+				@Override
+				public void onRemoved() {
+					mIsNetworkCroutonDisplayed = false;
+				}
+			});
+
+			networkCrouton.show();
+		}
 	}
 }
--- src/fr/utc/assos/uvweb/util/ConnectionUtils.java
+++ src/fr/utc/assos/uvweb/util/ConnectionUtils.java
@@ -3,6 +3,7 @@ package fr.utc.assos.uvweb.util;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.view.ViewGroup;
 import de.keyboardsurfer.android.widget.crouton.Style;
 
 /**
@@ -12,7 +13,7 @@ public class ConnectionUtils {
 	public static final Style NETWORK_ERROR_STYLE = new Style.Builder()
 			.setBackgroundColorValue(Style.holoRedLight)
 			.setDuration(2000)
-			.setTextSize(16)
+			.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT)
 			.build();
 
 	public static boolean isOnline(Context context) {
