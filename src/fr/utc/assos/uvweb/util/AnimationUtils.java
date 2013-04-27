a5df26b7072c9fe48b60d724cfa9ce7f110d328c
Thomas <thomas.keunebroek@gmail.com>
2013-04-27 17:09:27 +0200

    Tweak CardAnimation values a bit

--- src/fr/utc/assos/uvweb/util/AnimationUtils.java
+++ src/fr/utc/assos/uvweb/util/AnimationUtils.java
@@ -4,6 +4,6 @@ package fr.utc.assos.uvweb.util;
  * A helper class containing a few parameters for the application's animations.
  */
 public class AnimationUtils {
-	public static final long CARD_ANIMATION_DELAY_MILLIS = 50;
-	public static final long CARD_ANIMATION_DURATION_MILLIS = 750;
+	public static final long CARD_ANIMATION_DELAY_MILLIS = 100;
+	public static final long CARD_ANIMATION_DURATION_MILLIS = 800;
 }
