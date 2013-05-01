76f8b9c539f11baa00f89ab00671703154f9bee1
Thomas <thomas.keunebroek@gmail.com>
2013-05-01 16:57:13 +0200

    Update to latest Crouton rev

--- src/fr/utc/assos/uvweb/util/ConnectionUtils.java
+++ src/fr/utc/assos/uvweb/util/ConnectionUtils.java
@@ -3,18 +3,13 @@ package fr.utc.assos.uvweb.util;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.view.ViewGroup;
 import de.keyboardsurfer.android.widget.crouton.Style;
 
 /**
  * A helper class used to determine the current network state, i.e. online or not online
  */
 public class ConnectionUtils {
-	public static final Style NETWORK_ERROR_STYLE = new Style.Builder()
-			.setBackgroundColorValue(Style.holoRedLight)
-			.setDuration(2000)
-			.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT)
-			.build();
+	public static final Style NETWORK_ERROR_STYLE = Style.ALERT;
 
 	public static boolean isOnline(Context context) {
 		final ConnectivityManager connMgr = (ConnectivityManager)
