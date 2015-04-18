3708e040c5f2814c8797659e60c72f1e6a4df7f9
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-04-18 11:01:13 +0200

    Drop everything

--- src/fr/utc/assos/uvweb/util/AnimationUtils.java
+++
@@ -1,9 +0,0 @@
-package fr.utc.assos.uvweb.util;
-
-/**
- * A helper class containing a few parameters for the application's animations.
- */
-public final class AnimationUtils {
-	public static final long CARD_ANIMATION_DELAY_MILLIS = 50;
-	public static final long CARD_ANIMATION_DURATION_MILLIS = 450;
-}
