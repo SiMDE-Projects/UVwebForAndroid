3a0431ed902b784ce34cb14364991317bf9b9092
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-04-19 15:48:06 +0200

    Save instance state (newsfeed)

--- app/src/main/java/fr/utc/assos/uvweb/model/NewsfeedItem.java
+++ app/src/main/java/fr/utc/assos/uvweb/model/NewsfeedItem.java
@@ -1,8 +1,11 @@
 package fr.utc.assos.uvweb.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
-public class NewsfeedItem {
+public class NewsfeedItem implements Parcelable {
     @SerializedName("identity")
     private String author;
     private String date;
@@ -10,6 +13,14 @@ public class NewsfeedItem {
     private String uvName;
     private int globalRate;
     private String comment;
+
+    public NewsfeedItem(Parcel in) {
+        author = in.readString();
+        date = in.readString();
+        uvName = in.readString();
+        globalRate = in.readInt();
+        comment = in.readString();
+    }
 
     public String getAuthor() {
         return author;
@@ -30,4 +41,28 @@ public class NewsfeedItem {
     public String getComment() {
         return comment;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(author);
+        dest.writeString(date);
+        dest.writeString(uvName);
+        dest.writeInt(globalRate);
+        dest.writeString(comment);
+    }
+
+    public static final Parcelable.Creator<NewsfeedItem> CREATOR = new Parcelable.Creator<NewsfeedItem>() {
+        public NewsfeedItem createFromParcel(Parcel in) {
+            return new NewsfeedItem(in);
+        }
+
+        public NewsfeedItem[] newArray(int size) {
+            return new NewsfeedItem[size];
+        }
+    };
 }
--- app/src/main/java/fr/utc/assos/uvweb/ui/NewsFeedFragment.java
+++ app/src/main/java/fr/utc/assos/uvweb/ui/NewsFeedFragment.java
@@ -1,6 +1,7 @@
 package fr.utc.assos.uvweb.ui;
 
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,16 +11,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import fr.utc.assos.uvweb.R;
 import fr.utc.assos.uvweb.api.UvwebProvider;
 import fr.utc.assos.uvweb.model.Newsfeed;
+import fr.utc.assos.uvweb.model.NewsfeedItem;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
 public class NewsfeedFragment extends Fragment implements Callback<Newsfeed> {
     private static final String TAG = NewsfeedFragment.class.getSimpleName();
+    private static final String STATE_NEWSFEED_ITEMS = "state_newsfeed_items";
+
     private NewsfeedAdapter adapter;
+    private List<NewsfeedItem> items;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -34,14 +42,33 @@ public class NewsfeedFragment extends Fragment implements Callback<Newsfeed> {
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        UvwebProvider.getNewsfeed(this);
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if (savedInstanceState == null) {
+            UvwebProvider.getNewsfeed(this);
+        } else {
+            items = savedInstanceState.getParcelableArrayList(STATE_NEWSFEED_ITEMS);
+            updateViews();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (items != null) {
+            outState.putParcelableArrayList(STATE_NEWSFEED_ITEMS, new ArrayList<Parcelable>(items));
+        }
     }
 
     @Override
     public void success(Newsfeed newsfeed, Response response) {
-        adapter.setItems(newsfeed.getItems());
+        items = newsfeed.getItems();
+        updateViews();
+    }
+
+    private void updateViews() {
+        adapter.setItems(items);
     }
 
     @Override
