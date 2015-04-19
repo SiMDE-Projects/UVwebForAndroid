a41d5d90167b90c541e2a2ec3d83dc2d3ea28ff1
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-04-19 18:51:01 +0200

    Create UV fragment

--- app/src/main/java/fr/utc/assos/uvweb/ui/UvActivity.java
+++ app/src/main/java/fr/utc/assos/uvweb/ui/UvActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 
+import fr.utc.assos.uvweb.R;
 import fr.utc.assos.uvweb.model.UvListItem;
 
 public class UvActivity extends ActionBarActivity {
@@ -12,6 +13,8 @@ public class UvActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_uv);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -23,6 +26,7 @@ public class UvActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             finish();
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
---
+++ app/src/main/java/fr/utc/assos/uvweb/ui/UvFragment.java
@@ -0,0 +1,16 @@
+package fr.utc.assos.uvweb.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fr.utc.assos.uvweb.R;
+
+public class UvFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_uv, container, false);
+    }
+}
