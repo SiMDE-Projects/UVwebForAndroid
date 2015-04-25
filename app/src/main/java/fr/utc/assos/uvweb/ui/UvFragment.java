75ac8790f4ffe0367b0e37d5b513fbf1acb7818f
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-04-25 18:42:50 +0200

    Create recycler for UV detail comments

--- app/src/main/java/fr/utc/assos/uvweb/ui/UvFragment.java
+++ app/src/main/java/fr/utc/assos/uvweb/ui/UvFragment.java
@@ -2,6 +2,8 @@ package fr.utc.assos.uvweb.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,11 @@ import fr.utc.assos.uvweb.R;
 public class UvFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_uv, container, false);
+        View root =  inflater.inflate(R.layout.fragment_uv, container, false);
+
+        RecyclerView recycler = (RecyclerView) root.findViewById(R.id.recycler);
+        recycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return root;
     }
 }
