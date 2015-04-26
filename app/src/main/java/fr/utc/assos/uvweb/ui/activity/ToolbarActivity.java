a36b3e2fefddf17445c253a5b0f29dffd69c7c5c
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-04-26 18:43:37 +0200

    Update support library

--- app/src/main/java/fr/utc/assos/uvweb/ui/activity/ToolbarActivity.java
+++ app/src/main/java/fr/utc/assos/uvweb/ui/activity/ToolbarActivity.java
@@ -2,11 +2,12 @@ package fr.utc.assos.uvweb.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import fr.utc.assos.uvweb.R;
 
-public abstract class ToolbarActivity extends ActionBarActivity {
+public abstract class ToolbarActivity extends AppCompatActivity {
     private Toolbar toolbar;
 
     @Override
