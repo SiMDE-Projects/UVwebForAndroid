56214b76c9b39ee363ee000eb8c3bdc6fa5315fb
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-04-28 19:54:38 +0200

    Make comment activity cloasble

--- app/src/main/java/fr/utc/assos/uvweb/ui/activity/CommentActivity.java
+++ app/src/main/java/fr/utc/assos/uvweb/ui/activity/CommentActivity.java
@@ -1,11 +1,29 @@
 package fr.utc.assos.uvweb.ui.activity;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import fr.utc.assos.uvweb.R;
 
 public class CommentActivity extends ToolbarActivity {
 
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
     protected int getLayoutId() {
         return R.layout.activity_comment;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
