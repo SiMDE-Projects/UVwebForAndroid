388027ce7a676489ffbf3ec7ad7c7a80e6f95ca9
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-04-29 20:59:03 +0200

    Remove unused import

--- app/src/main/java/fr/utc/assos/uvweb/ui/activity/ToolbarActivity.java
+++ app/src/main/java/fr/utc/assos/uvweb/ui/activity/ToolbarActivity.java
@@ -1,7 +1,6 @@
 package fr.utc.assos.uvweb.ui.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
