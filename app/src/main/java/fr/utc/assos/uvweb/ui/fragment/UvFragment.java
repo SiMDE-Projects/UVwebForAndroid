817859fe66e45e8fb876ce5fb0dcd7cccf876fd8
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-05-15 08:58:17 +0200

    Remove unused import

--- app/src/main/java/fr/utc/assos/uvweb/ui/fragment/UvFragment.java
+++ app/src/main/java/fr/utc/assos/uvweb/ui/fragment/UvFragment.java
@@ -10,7 +10,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
